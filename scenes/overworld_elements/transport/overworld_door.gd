9214ab069580a5aec9d6c7d8fabe73765eb6f4fa
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2023-11-11 12:32:09 -0600

    door stable

--- scenes/overworld_elements/transport/overworld_door.gd
+++ scenes/overworld_elements/transport/overworld_door.gd
@@ -6,28 +6,30 @@ class_name OverworldDoor
 @export var player_group := "player"
 @export var safety_disable_time := 1.0
 
+var deactivated := false
+
 func _ready() -> void:
 	# little safety to prevent getting stuck in a portal loop
 	monitoring = false; # monitoring is managed by level loading logic
 	GameManager.level_finished_loading.connect(do_enter, CONNECT_ONE_SHOT | CONNECT_DEFERRED)
 
 func do_enter() -> void:
+	monitoring = true
 	if GameManager.current_door_target == name:
+		deactivated = true
 		body_exited.connect(_exit_handler)
 		var player := get_tree().get_first_node_in_group(player_group) as Node2D
 		player.global_position = global_position
-	else:
-		monitoring = true
 
 func _exit_handler(body : Node2D) -> void:
 	if not body.is_in_group(player_group):
 		return
-	monitoring = true
+	deactivated = false
 	body_exited.disconnect(_exit_handler)
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if not body.is_in_group(player_group):
+	if deactivated or not body.is_in_group(player_group):
 		return
 	GameManager.current_door_target = door_target
 	GameManager.viewport.load_scene(target_scene)
