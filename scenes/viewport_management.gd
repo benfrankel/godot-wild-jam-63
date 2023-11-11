e05888af2804a90dc07aabbf2da6249c3deaa428
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2023-11-11 10:20:04 -0600

    Ported game to use a coherent viewport structure; custom level loading

---
+++ scenes/viewport_management.gd
@@ -0,0 +1,35 @@
+extends Node
+class_name ViewportManager
+
+@export_file("*.tscn") var main_scene := ""
+
+@export var transition_time := 0.2
+
+@onready var pixel_level_root := $PixelScene/SubViewport
+@onready var hi_res_gui_root := $HiResGUIRoot
+
+@onready var trans := $TransitionFX
+
+func _ready() -> void:
+	GameManager.viewport = self
+	trans.modulate.a = 1.0
+	load_scene(main_scene)
+
+func pause(is_paused :bool) -> void:
+	get_tree().paused = is_paused
+
+## manages a sequence for the 
+func load_scene(path : String) -> void:
+	pause(true)
+	await _fade(1.0)
+	for c in pixel_level_root.get_children():
+		c.queue_free() # should only ever be one, but let's manage edge cases
+	var scene := (load(path) as PackedScene).instantiate() # may cause stutter here for large levels.
+	pixel_level_root.add_child(scene)
+	await _fade(0.0)
+	pause(false)
+
+func _fade(target :float = 1.0) -> void:
+	var t := get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	t.tween_property(trans, "modulate:a", target, transition_time)
+	await t.finished # makes this func awaitable
