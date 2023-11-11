3e7f65fc6b607d401b6cdf7cba2a44a80cd5cc7f
Ben Frankel <ben.frankel7@gmail.com>
2023-11-10 17:19:53 -0800

    Add barebones projectile

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,9 @@
+class_name Player
 extends CharacterBody2D
 
+
+@export
+var health := 10.0
 
 @export_range(0.0, 1000.0)
 var speed := 300.0
@@ -8,4 +12,4 @@ var speed := 300.0
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
 	self.velocity = self.speed * direction
-	move_and_slide()
+	self.move_and_slide()
