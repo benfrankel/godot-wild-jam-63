5dab777d25ba0fb5e008aef7d20ae6c5b655a85f
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2023-11-13 16:31:34 -0600

    basic combat overworld detection

---
+++ scenes/overworld/components/overworld_combat_area.gd
@@ -0,0 +1,41 @@
+extends Area2D
+class_name CombatArea
+"""
+Translates any arbitrary collider (including CollisionPolygon2D) into a grid of cells where each cell triggers a new rng check for starting a fight. This also allows different areas to have different fights available
+"""
+
+
+@export var cell_size := 32.0
+@export_range(0, 1) var chance_combat_per_cell := 0.2
+
+var last_cell := Vector2.ONE * -1
+var target_body : Node2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_enter)
+	body_exited.connect(_on_body_exit)
+
+func _physics_process(_delta: float) -> void:
+	if not target_body:
+		return
+	var curr_cell := _get_cell(target_body.global_position)
+	if curr_cell != last_cell:
+		last_cell = curr_cell
+		_try_do_combat()
+
+func _try_do_combat() -> void:
+	if randf() < chance_combat_per_cell:
+		print("Doing combat!!!!!")
+	else:
+		print("did not do combat")
+
+func _get_cell(global_pos : Vector2) -> Vector2:
+	return (global_pos / cell_size).floor()
+
+func _on_body_enter(body : Node2D) -> void:
+	if body.is_in_group(GameManager.PLAYER_GROUP):
+		target_body = body
+
+func _on_body_exit(body : Node2D) -> void:
+	if body == target_body:
+		target_body = null
