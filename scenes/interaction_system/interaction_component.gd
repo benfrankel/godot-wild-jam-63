6dccfa805c6f6ae449c1c5ca6ae74308a5f7a130
Ben Frankel <ben.frankel7@gmail.com>
2023-11-13 01:05:55 -0800

    Fix losing interaction target

--- scenes/interaction_system/interaction_component.gd
+++ scenes/interaction_system/interaction_component.gd
@@ -1,11 +1,10 @@
-extends Area2D
 class_name InteractionComponent
+extends Area2D
+## Simple component that can receive an interact call and emits a signal for other component(s) to handle.
 
-"""
-Simple component that can receive an interact call and emits a signal for other component(s) to handle
-"""
 
 signal interacted_with
+
 
 func interact() -> void:
 	interacted_with.emit()
--- scenes/interaction_system/interaction_sensor.gd
+++ scenes/interaction_system/interaction_sensor.gd
@@ -1,35 +1,29 @@
 extends RayCast2D
-class_name InteractionSensor
-
-## emitted when a new interaction is found and it is not the previously found interactable
-signal interaction_available(name: String)
-## emitted when interaction is no longer available
-signal interaction_unavailable
-
-## the last object to be found that is interactable
-var last_interactable :Node = null
 
 
-func _ready() -> void:
-	collide_with_areas = true
+## emitted when a new interaction is found
+signal target_found(target: Node)
+## emitted when an interaction is no longer available
+signal target_lost
+
+## the current target interaction, or null
+var target: InteractionComponent
+
 
 func _physics_process(_delta: float) -> void:
-	if not is_colliding():
-		return
-	var coll := get_collider()
-	if "interact" in coll:
-		if coll != last_interactable:
-			interaction_available.emit(coll.name)
-			last_interactable = coll
-	elif last_interactable:
-		last_interactable = null
-		interaction_unavailable.emit()
+	var seen = get_collider() as InteractionComponent
+	if target and not seen:
+		target = null
+		target_lost.emit()
+	elif target != seen:
+		target = seen
+		target_found.emit(target)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not enabled:
 		return
-	if event.is_action_pressed("interact") and last_interactable:
-		last_interactable.interact() # safety checked in physics process
+	if target and event.is_action_pressed("interact"):
+		target.interact()
 		get_viewport().set_input_as_handled()
 		
