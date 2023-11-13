c73702ffc0737da4c046f9f7d804432cea4d6413
Ben Frankel <ben.frankel7@gmail.com>
2023-11-13 12:39:15 -0800

    Make Attack1 a LinearProjectile

--- scenes/combat/projectile/linear_projectile.gd
+++ scenes/combat/projectile/linear_projectile.gd
@@ -1,4 +1,9 @@
+class_name LinearProjectile
 extends Projectile
+
+
+@export_range(0.0, 1000.0) var speed := 300.0
+@export_range(0.0, TAU) var angle := 0.0
 
 
 func _physics_process(delta: float):
--- scenes/combat/projectile/projectile.gd
+++ scenes/combat/projectile/projectile.gd
@@ -3,8 +3,6 @@ extends Area2D
 
 
 @export var damage := 1.0
-@export_range(0.0, 2000.0) var speed := 800.0
-@export_range(0.0, TAU) var angle := 0.0
 
 
 func _on_body_entered(body: Node2D):
