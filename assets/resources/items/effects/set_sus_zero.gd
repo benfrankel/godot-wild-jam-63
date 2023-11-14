40a8171ce1bd06ec3c6a919c65faf93b39183b8c
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2023-11-14 13:54:58 -0600

    created example item and item effect for Catnip

---
+++ assets/resources/items/effects/set_sus_zero.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func apply(game_state : Variant) -> void:
+	push_warning("Not yet implemented")
+	print("Script for setting combat suspicion to zero is not currently implemented. Please review the warning and implement the game state effect")
