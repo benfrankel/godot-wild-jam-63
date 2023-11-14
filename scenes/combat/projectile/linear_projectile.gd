b0337e58fc77947d940d0fb93226600a118b4755
Ben Frankel <ben.frankel7@gmail.com>
2023-11-13 17:22:31 -0800

    Add initial wait to projectiles

--- scenes/combat/projectile/linear_projectile.gd
+++ scenes/combat/projectile/linear_projectile.gd
@@ -2,7 +2,18 @@ class_name LinearProjectile
 extends Projectile
 
 
+var is_moving := false
+
+
+func _ready() -> void:
+	await get_tree().create_timer(wait).timeout
+	is_moving = true
+
+
 func _physics_process(delta: float):
+	if not is_moving:
+		return
+
 	var direction := Vector2.from_angle(angle)
 	var velocity := speed * direction
 	position += delta * velocity
--- scenes/combat/projectile/projectile.gd
+++ scenes/combat/projectile/projectile.gd
@@ -2,5 +2,9 @@ class_name Projectile
 extends Area2D
 
 
-@export_range(0.0, 1000.0) var speed := 300.0
-@export_range(0.0, TAU) var angle := 0.0
+## Time in seconds before the projectile starts moving.
+@export var wait := 0.0
+## Initial speed of the projectile.
+@export var speed := 300.0
+## Initial angle of the projectile.
+@export var angle := 0.0
