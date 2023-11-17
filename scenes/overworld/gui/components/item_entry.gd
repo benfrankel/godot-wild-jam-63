81704c636aa23f4c56941c327cc865fdbb0ba935
Ben Frankel <ben.frankel7@gmail.com>
2023-11-17 10:48:20 -0800

    Add item icons & descriptions

--- scenes/overworld/gui/components/item_entry.gd
+++ scenes/overworld/gui/components/item_entry.gd
@@ -1,17 +1,23 @@
-extends HBoxContainer
 class_name ItemEntry
+extends HBoxContainer
+
 
 signal action_button_pressed
 
-func set_item(item : Item) -> void:
+
+func set_item(item: Item) -> void:
 	$Icon.texture = item.texture
 	$Label.text = item.item_name
+	tooltip_text = item.description
 
-func set_action_name(action_name : String) -> void:
+
+func set_action_name(action_name: String) -> void:
 	$BtnAction.text = action_name
+
 
 func set_no_action() -> void:
 	$BtnAction.queue_free()
+
 
 func _on_btn_action_pressed() -> void:
 	action_button_pressed.emit()
--- scripts/resource_types/Item.gd
+++ scripts/resource_types/Item.gd
@@ -1,20 +1,24 @@
-extends Resource
 class_name Item
+extends Resource
+
 
 @export var item_name := ""
-@export var texture : Texture2D
-@export var effects : Array[Script] = []
+@export var description := ""
+@export var texture: Texture2D
+@export var effects: Array[Script] = []
 @export var is_boss_item := false
+
 
 func is_consumable() -> bool:
 	return not effects.is_empty()
-	
-func apply_effects(game_state : CombatState) -> void:
+
+
+func apply_effects(combat_state: CombatState) -> void:
 	for eff in effects:
 		var node := Node.new()
 		node.set_script(eff)
 		if "apply" in node:
-			node.apply(game_state)
-			game_state.emit_changed()
+			node.apply(combat_state)
+			combat_state.emit_changed()
 		else:
 			push_warning("Invalid item effect found: [ %s ] does not implement 'apply'" % eff.effect_script.resource_path )
