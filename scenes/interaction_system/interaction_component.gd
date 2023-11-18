92902494bc3ee6c58fcf28c235562c85e29020e3
Ben Frankel <ben.frankel7@gmail.com>
2023-11-18 12:28:28 -0800

    Support npcs giving you gifts

--- scenes/interaction_system/interaction_component.gd
+++ scenes/interaction_system/interaction_component.gd
@@ -8,6 +8,7 @@ signal interacted_with
 
 @export var auto_trigger := false
 @export var dialog: DialogData
+@export var gift: Array[Item]
 @export var enemy: Enemy
 
 
@@ -24,5 +25,8 @@ func interact() -> void:
 	interacted_with.emit()
 	if dialog:
 		await GameManager.do_dialog(Dialog.create_dialog(dialog))
+	if gift:
+		await GameManager.viewport.do_loot_screen(LootScreen.create(Inventory.from_items(gift.duplicate())))
+		gift.clear()
 	if enemy:
 		GameManager.enter_combat(enemy)
