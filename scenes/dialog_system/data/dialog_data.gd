71027caae4ae64457bc0dabf2dd34dbbee85001b
Ben Frankel <ben.frankel7@gmail.com>
2023-11-18 12:34:52 -0800

    Add a generic NPC

--- scenes/dialog_system/data/dialog_data.gd
+++ scenes/dialog_system/data/dialog_data.gd
@@ -3,6 +3,6 @@ class_name DialogData
 
 @export var character_name : String = ""
 @export var text_sound : AudioStream
-@export var text_speed := 30.0
+@export var text_speed := 40.0
 @export var auto_progress := false
 @export var lines : Array[String] = []
