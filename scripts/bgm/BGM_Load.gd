8a7e88429743e471742ff3e48e3ea3b910858a13
Ben Frankel <ben.frankel7@gmail.com>
2023-11-18 15:25:05 -0800

    Adjust cross-fade

--- scripts/bgm/BGM_Load.gd
+++ scripts/bgm/BGM_Load.gd
@@ -2,13 +2,14 @@ extends Node
 class_name BGM_Load
 
 @export var music : AudioStream
-@export var crossfade := 5.0
-@export var reset_on_end := true
+@export var crossfade := 0.5
+@export var volume := 0.0
+@export var reset_on_end := false
 
 func _ready() -> void:
-	BGM.play_stream(music, crossfade)
+	BGM.play_stream(music, crossfade, volume)
 
 func _exit_tree() -> void:
 	if not reset_on_end:
 		return
-	BGM.play_stream(BGM.background.stream, crossfade)
+	BGM.play_stream(BGM.background.stream, crossfade, volume)
--- scripts/globals/bgm.gd
+++ scripts/globals/bgm.gd
@@ -5,22 +5,25 @@ extends Node
 
 var tween : Tween
 
-const PLAYING_VOLUME := 0.0
 const SILENCE_VOLUME := -80.0
+const QUIET_VOLUME := -20.0
 
-func play_stream(audio : AudioStream, duration :float = 0.5) -> void:
-	background.stop()
-	background.stream = audio
+func play_stream(audio: AudioStream, duration := 0.5, volume := 0.0) -> void:
 	if tween:
 		tween.kill()
+
 	# swap
 	var temp := background
 	background = active
 	active = temp
 
-	active.play(0)
+	active.stop()
+	active.stream = audio
+	active.volume_db = QUIET_VOLUME
+
 	tween = get_tree().create_tween()
-	tween.tween_property(active, "volume_db", PLAYING_VOLUME, duration)
-	tween.parallel().tween_property(background, "volume_db", SILENCE_VOLUME, duration)
+	tween.tween_property(background, "volume_db", QUIET_VOLUME, duration / 2.0)
+	tween.tween_callback(Callable(active, "play"))
+	tween.tween_property(background, "volume_db", SILENCE_VOLUME, duration / 2.0)
+	tween.parallel().tween_property(active, "volume_db", volume, duration)
 	tween.tween_callback(Callable(background, "stop"))
-	
