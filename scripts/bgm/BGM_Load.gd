250c0376e754f7e173cd564e10327b3f4ecd592a
Ben Frankel <ben.frankel7@gmail.com>
2023-11-18 16:24:47 -0800

    Don't reset bg music if the next room uses the same track

--- scripts/bgm/BGM_Load.gd
+++ scripts/bgm/BGM_Load.gd
@@ -7,9 +7,9 @@ class_name BGM_Load
 @export var reset_on_end := false
 
 func _ready() -> void:
-	BGM.play_stream(music, crossfade, volume)
+	if BGM.active.stream != music:
+		BGM.play_stream(music, crossfade, volume)
 
 func _exit_tree() -> void:
-	if not reset_on_end:
-		return
-	BGM.play_stream(BGM.background.stream, crossfade, volume)
+	if reset_on_end:
+		BGM.play_stream(BGM.background.stream, crossfade, volume)
